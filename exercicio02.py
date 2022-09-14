01840d468ca34bd3e13f4f6f8246c6f1b3d71a9d
nicolas lira <86638443+LiraNick@users.noreply.github.com>
2022-09-13 21:28:09 -0300

    Update exercicio02.py

--- exercicio02.py
+++ exercicio02.py
@@ -3,7 +3,7 @@ poli = "="*15
 print(f"\n {poli} CABEÇALHO {poli} \n")
 
 for c in range(10):
-    print(" "*9, "FELIZ DIA DO PROGRAMADOR")
+    print(f"\n {poli} FELIZ DIA DO PROGRAMADOR {poli} \n")
 
 
 print(f"\n {poli} RODAPE {poli} \n")
